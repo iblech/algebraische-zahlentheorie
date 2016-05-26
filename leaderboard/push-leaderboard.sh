8003d6dce70e328b786bb249dd4321b4dbb57865
Ingo Blechschmidt <iblech@web.de>
2016-05-26 15:15:27 +0200

    Neue Aufrufsyntax

--- leaderboard/push-leaderboard.sh
+++ leaderboard/push-leaderboard.sh
@@ -4,7 +4,7 @@
 ssh speicherleck.de "
   cd /var/www/iblech/stuff/tutor-algebraische-zahlentheorie/leaderboard || exit
   git pull --rebase
-  cabal build
+  nix-shell --run 'cabal build' -p 'haskellPackages.ghcWithPackages (pkgs: with pkgs; [blaze-html shakespeare])'
   cd ..
   ./leaderboard/dist/build/Main/Main > index.html
   pdfjoin --outfile alle.pdf uebung??.pdf
