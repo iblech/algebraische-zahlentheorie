2f0a73bccc82fc8848d610cf26212c0d1300b7e4
Ingo Blechschmidt <iblech@web.de>
2016-07-21 00:31:50 +0200

    Weitere Abgaben

--- leaderboard/Main.hs
+++ leaderboard/Main.hs
@@ -124,6 +124,5 @@ $doctype 5
         <a href="uebung#{format n}.pdf">Blatt #{show n}
         <br>
       <a href="alle.pdf">Alle Übungsblätter in einer Datei
-    <p class="moral" style="background-color: #dd434b; color: white; font-weight: bold">
-      Bitte Kommentar im <a href="https://etherpad.wikimedia.org/p/RPUjEpIKmv">Kummerkasten</a> abgeben! Vielen Dank. :-)
+    <img src="images/schrei.jpeg" alt="\o/">
 |]
